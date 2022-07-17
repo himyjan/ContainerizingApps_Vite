be4ce630f46d8441fe7b1da4711fccf9e218c853
Your Name <you@example.com>
2022-07-17 20:23:41 +0800

    update package version

--- reacttypescript/src/App.tsx
+++ reacttypescript/src/App.tsx
@@ -1,55 +1,26 @@
-import normalize from 'emotion-normalize';
-import { css, Global } from '@emotion/react';
-import { GlobalPortal } from './GlobalPortal';
-import { Router } from 'pages/Routes';
-import { ReactNode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <GlobalPortal.Provider>
-        <Global
-          styles={css`
-            ${normalize}
-            h1, h2, h3, h4, h5, h6 {
-              font-size: 1em;
-              font-weight: normal;
-              margin: 0; /* or ‘0 0 1em’ if you’re so inclined */
-            }
-            * {
-              box-sizing: border-box;
-              padding: 0;
-              margin: 0;
-            }
-
-            html,
-            body,
-            #root {
-              height: 100%;
-            }
-          `}
-        />
-        <Layout>
-          <Router />
-        </Layout>
-      </GlobalPortal.Provider>
-    </BrowserRouter>
-  );
-}
-
-function Layout({ children }: { children: ReactNode }) {
-  return (
-    <div
-      css={css`
-        max-width: 100%;
-        width: 100%;
-        padding: 0;
-        margin: 0;
-        height: auto;
-      `}
-    >
-      <div css={css``}>{children}</div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
